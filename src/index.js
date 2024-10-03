e51a0ec14a0cc43c98637bbe5aa710783604a054
Pavani <65592925+Pavani38@users.noreply.github.com>
2024-10-03 21:28:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,3 @@ import { render } from "react-dom";
 import App from "./App";
 
 render(<App></App>, document.getElementById("app"));
-
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
